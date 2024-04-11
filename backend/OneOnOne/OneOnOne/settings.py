e5c7e85aee1c536dcb5d1d62d5e5ff13ec504f2d
robinrswanson <robinr.swanson@mail.utoronto.ca>
2024-04-10 21:31:37 -0400

    Settings

--- backend/OneOnOne/OneOnOne/settings.py
+++ backend/OneOnOne/OneOnOne/settings.py
@@ -92,6 +92,7 @@ SIMPLE_JWT = {
 }
 
 
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -144,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
